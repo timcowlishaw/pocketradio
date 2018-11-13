f7c556852f8abc28423f9b5538c6bafaa95b4777
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2018-11-13 18:53:03 +0000

    blurg

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ def get_podcast_files():
     pocketcasts = Pocketcasts(os.environ["POCKETCASTS_USERNAME"], password=os.environ["POCKETCASTS_PASSWORD"])
     podcasts = [e.url for p in pocketcasts.get_subscribed_podcasts() for e in pocketcasts.get_podcast_episodes(p)]
     random.shuffle(podcasts)
-    return Response(("\n").join(podcasts), mimetype="audio/x-mpegurl")
+    return Response(("\n").join(podcasts)[0:100], mimetype="audio/x-mpegurl")
 
 if __name__ == "__main__":
     app.run()
