939ee10acbf8edff76ac021ad10e44aee36141c2
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2018-11-13 18:50:44 +0000

    fix shit

--- application.py
+++ application.py
@@ -1,22 +1,18 @@
-from flask import Flask
+from flask import Flask, Response
 from pocketcasts import Pocketcasts
 import os
 import random
+
+
 app = Flask(__name__)
-pocket = Pocketcasts(os.environ["POCKETCASTS_USERNAME"], password=os.environ["POCKETCASTS_PASSWORD"])
+
 @app.route('/')
 def get_podcast_files():
-    return 
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
+    pocketcasts = Pocketcasts(os.environ["POCKETCASTS_USERNAME"], password=os.environ["POCKETCASTS_PASSWORD"])
     podcasts = [e.url for p in pocketcasts.get_subscribed_podcasts() for e in pocketcasts.get_podcast_episodes(p)]
     random.shuffle(podcasts)
-    return ("\n").join(podcasts)
+    return Response(("\n").join(podcasts), mimetype="audio/x-mpegurl")
 
+if __name__ == "__main__":
+    app.run()
 
