0a801173a951b540440134726e241b6581a4627b
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2018-11-14 12:40:47 +0000

    bums

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def get_podcast_files():
     pocketcasts = Pocketcasts(os.environ["POCKETCASTS_USERNAME"], password=os.environ["POCKETCASTS_PASSWORD"])
-    podcasts = [e.url for p in pocketcasts.get_subscribed_podcasts() for e in pocketcasts.get_podcast_episodes(p) if e.played_up_to() == 0]
+    podcasts = [e.url for p in pocketcasts.get_subscribed_podcasts() for e in pocketcasts.get_podcast_episodes(p) if e.played_up_to == 0]
     random.shuffle(podcasts)
     return Response(("\n").join(podcasts[0:100]), mimetype="audio/x-mpegurl")
 
